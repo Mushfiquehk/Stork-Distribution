b3ed9794e49cf11f6fe766a69fb065b6fe1337b3
Mushfiquehk <mushfiquehasankhan@gmail.com>
2022-06-04 17:15:09 -0500

    debug mode turned on

--- stork/settings.py
+++ stork/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://storkdistro.herokuapp.com/']
 
 SECURE_SSL_REDIRECT = False
 
