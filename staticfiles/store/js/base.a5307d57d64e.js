206f8835eb70ce23992a66f4c552457f94af1ef1
Mushfiquehk <mushfiquehasankhan@gmail.com>
2023-12-30 13:29:42 -0600

    'removed collected static

--- staticfiles/store/js/base.a5307d57d64e.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  var menuDrop = document.getElementById('menu-dropdown');
-  var menuItems= document.getElementById('menu-items');
-  
-  menuDrop.addEventListener('click', () => {
-    menuItems.classList.toggle('hidden')
-  })
-})
